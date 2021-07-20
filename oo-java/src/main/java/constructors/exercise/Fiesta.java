e76f144cf91be029a35aacf47a61084200d0e1d8
Nadia <nlnadialigia@gmail.com>
2021-07-20 11:22:56 -0300

    refactor: fix class Fiesta errors

--- oo-java/src/main/java/constructors/exercise/Fiesta.java
+++ oo-java/src/main/java/constructors/exercise/Fiesta.java
@@ -5,7 +5,9 @@ public class Fiesta {
   public static void main(String[] args) {
 
     Car fiesta = new Car("Volskwagen", "Hatch", 2015);
-    
-    System.out.print(fiesta.getBrand() + fiesta.getModel() + fiesta.getYear());
+
+    System.out.printf("Marca: %s \n", fiesta.getBrand());
+    System.out.printf("Modelo: %s \n", fiesta.getModel());
+    System.out.printf("Ano: %s", fiesta.getYear());
   }
 }
